a2be461b104f3bbaf8a10a2a3ea3699083f362e8
SRUSHTI52 <144897696+SRUSHTI52@users.noreply.github.com>
2024-07-04 18:59:29 +0530

    responsive

--- vitereactproj/src/App.jsx
+++ vitereactproj/src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
+
 import './App.css'
 import Weather from './Weather';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='container'>
